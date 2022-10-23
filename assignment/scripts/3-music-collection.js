64c75d8afa68b878807c93ed7f7d7080af249bbc
Myles Voigt <myles.voigt@gmail.com>
2022-10-22 20:43:09 -0500

    addToCollection function added

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    let album = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
